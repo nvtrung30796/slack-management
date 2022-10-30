9d6eec4462a544a5016f4abbb4918e03f6d9d7d9
Trung Nguyen <nvtrung30796@gmail.com>
2022-10-30 09:10:17 +0700

    Update vercel.json,next.config.js, callApi.ts files

--- services/callApi.ts
+++ services/callApi.ts
@@ -4,7 +4,7 @@ import {APP_CONFIG} from "../src/config/app.constant";
 export const callApi = (endpoint: string, method: string, body: any) => {
 
   return axios({
-    url: APP_CONFIG.HOST + "/api" + endpoint,
+    url: (APP_CONFIG.HOST || window.location.origin) + "/api" + endpoint,
     method: method ? method : "GET",
     data: body
   }).then(res => res)
